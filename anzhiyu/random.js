9ea8f89548c69bbdfabc1bc2886222591e6c7820
unknown <xxx>
2025-06-21 22:39:33 +0800

    Site updated: 2025-06-21 22:39:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/5634cdad.html","posts/269d3d81.html","posts/cb6ed29f.html","posts/351ffcb9.html","posts/ccff787.html","posts/c27aca5d.html","posts/424a780e.html"];function toRandomPost(){
+var posts=["posts/cb6ed29f.html","posts/5634cdad.html","posts/269d3d81.html","posts/823786d8.html","posts/351ffcb9.html","posts/424a780e.html","posts/c27aca5d.html","posts/52f301e9.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
